a58dbe6310ef90faf68b7bc7c47891b4f1064620
youngyang17 <yy2457@columbia.edu>
2018-03-22 23:58:56 -0400

    commit message

--- js/a5-1.js
+++ js/a5-1.js
@@ -1,8 +1,11 @@
-
 // ***** Global variables ***** //
+var deg;
+var mul;
 var score = [10.37,11.11,13.237,14.626,14.88,14.94,15.301,15.35,15.394,18.486,21.26,23.44,23.81,
-  24.05,24.4518,25.06,26.32,27.8,29.81,31.23,31.724,32.003,33.92,44.,47.08,49.12];
-var movie = ['SoHo-TriBeCa-Civic', 'Center-Little Italy',
+24.05,24.4518,25.06,26.32,27.8,29.81,31.23,31.724,32.003,33.92,44.,47.08,49.12];
+var movie = [
+'SoHo-TriBeCa-Civic',
+'Center-Little Italy',
 'Central Harlem South',
 'Hamilton Heights',
 'West Village',
@@ -27,35 +30,55 @@ var movie = ['SoHo-TriBeCa-Civic', 'Center-Little Italy',
 'Turtle Bay-East Midtown',
 'Lenox Hill-Roosevelt Island',
 'Lincoln Square',
-'Battery Park City-Lower Manhattan'];
+'Battery Park City-Lower Manhattan'
+];
 
 // ***** Setup function ***** //
 function setup(){
-  createCanvas(1000, 1000);
+  
+  createCanvas(1000, 900);
   textAlign(LEFT, TOP);
   textSize(14);
   textFont('Roboto');
   console.log('Setup complete...');
+  
+  mul = 8;
 }
 
 // ***** Draw function ***** //
 function draw(){
+  meg = map(mouseX,0,1000, 0, 80);
+  deg = PI* meg/180;
   background(213, 245, 227);
   fill(80);
   noStroke();
+  // ellipse(200,200,10);
   // Drawing the bar chart
   for (var i = 0; i < score.length; i++) {
-
-    quad( 200 + 20 * [i],500, 212 + 20 * [i], 500, 224 + 20 * [i], 500 + score[i] *5,
-      212 + 20 * [i], 500+ score [i] *5)
-
-   }
-
-}
-  // Drawing the labels
+  // quad(
+  // 200 + 20 * i, 480,
+  // 212 + 20 * i, 480, 
+  // 224 + 20 * i, 480 - score[i] *5,
+  // 212 + 20 * i, 480 - score[i] *5
+  // )
+  
+  beginShape();
+  vertex(160 + 25*i, 480);
+  vertex(160 + 25*i + score[i]*cos(deg)*mul, 480 - score[i]*sin(deg)*mul);
+  vertex(170 + 25*i + score[i]*cos(deg)*mul, 480 - score[i]*sin(deg)*mul);
+  vertex(175 + 25*i + (score[i]-1.5)*cos(deg)*mul, 480 - (score[i]-1.5)*sin(deg)*mul);
+  vertex(175 + 25*i, 480);
+  endShape(CLOSE);
+  }
+  
+// Drawing the labels
   fill(20);
   for (var i = 0; i < movie.length; i ++) {
-    textAlign(top);
-    text(movie[i],20*i,500);
-  }
+    push();
+    translate(160 + 25*i,500);
+    rotate(PI/2);
+    textAlign(LEFT,BASELINE);
+    text(movie[i],0,0);
+    pop();
+    }
 }
