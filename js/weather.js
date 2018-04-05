cb5640e2cf715e7179df0f0cb019dd8787c72a81
youngyang17 <yy2457@columbia.edu>
2018-04-05 19:12:03 -0400

    Commit message

---
+++ js/weather.js
@@ -0,0 +1,59 @@
+// **** Global Variables ***** //
+var apiKey = '35875619346eeb8604023170b794b9b6';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+var weatherData;
+var button;
+var cityInput;
+var description = '';
+var temperature = 0;
+var humidity = 0;
+var pressure = 0;
+var c;
+
+// **** Setup Function ****** //
+function setup(){
+  createCanvas(500, 500);
+  button = select('#submit');
+  cityInput = select('#city');
+  button.mousePressed(queryAPI);
+  noLoop();
+}
+
+// **** Query API Function *** //
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&apikey=' + apiKey;
+  loadJSON(request, getWeatherData);
+}
+
+function getWeatherData(apiData){
+  weatherData = apiData;
+  description = weatherData.weather[0].description;
+  temperature = weatherData.main.temp;
+  humidity = weatherData.main.humidity;
+  pressure = weatherData.main.pressure;
+  print(weatherData);
+  redraw();
+}
+
+// **** Draw Function **** //
+function draw(){
+  background (555);
+
+  if (weatherData){
+    text('The current weather for ' + cityInput.value() + ' is:', 50, 50);
+    text(description, 80, 70);
+    text(temperature + ' F', 80, 90);
+
+    if (temperature >= 100) {
+      c = color('red');
+    }
+    else if (temperature > 90) {
+       c = color('green');
+    }
+    else if (temperature > 80) {
+        c = color('blue');}
+    fill(c); // Use color variable 'c' as fill color
+    noStroke(); // Don't draw a stroke around shapes
+    rect(80,100, 55, 55);
+      }
+    }
